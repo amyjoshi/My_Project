42799e4eb995268c51f2aa517e0d7b21008c604f
amyjoshi <amy1992joshi@gmail.com>
2016-08-11 15:37:35 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+<meta charset="UTF-8">
+<title>Document</title>
 <script type="text/javascript" src="jquery.min.js"></script>
 <script type="text/javascript" src="jquery.form.js"></script>
 <script type="text/javascript">
@@ -25,14 +25,14 @@ $(document).ready(function(){
 </head>
 <body>
   <form method="post" name="multiple_upload_form" id="multiple_upload_form" enctype="multipart/form-data" action="upload.php">
-    <input type="hidden" name="image_form_submit" value="1"/>
-    <label>Choose Image</label>
-    <input type="file" name="images[]" id="images" multiple >
-    <div class="uploading none">
-        <label>&nbsp;</label>
-        <input type="submit" value="Upload Image" name="submit">
-    </div>
-    <div id="images_preview"></div>
+  <input type="hidden" name="image_form_submit" value="1"/>
+  <label>Choose Image</label>
+  <input type="file" name="images[]" id="images" multiple >
+  <div class="uploading none">
+      <label>&nbsp;</label>
+      <input type="submit" value="Upload Image" name="submit">
+  </div>
+  <div id="images_preview"></div>
 </form>
 </body>
 </html>
